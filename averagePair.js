cf0ff8c4073359802997986fd7c6134cc856ee9c
eternal golden bread <codylewisbrock@gmail.com>
2019-11-02 15:12:58 -0700

    adds solution to averagePair.js

---
+++ averagePair.js
@@ -0,0 +1,23 @@
+//Use the multiple-pointers approach to take in an array and determine 
+//if there is a pair of numbers that averages to the target average
+
+function averagePair(arr, targetAvg){
+    let start = 0;
+    let end = arr.length - 1;
+    
+    while (start < end) {
+        let result = (arr[start] + arr[end])/2
+        if (result === targetAvg) {
+            return true;
+        } else if (result > targetAvg) {
+            end--;
+        } else if (result < targetAvg) {
+            start++;
+        }
+    } return false;
+}
+
+console.log(averagePair([1,2,3,4,5,6,7,8], 4));
+console.log(averagePair([1,3,8,12,28,89,111,701], 10));
+console.log(averagePair([1,3,5,7], 7));
+
