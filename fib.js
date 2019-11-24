c54712e3f682e9c479bb3e650e5e7b705b89709a
eternal golden bread <codylewisbrock@gmail.com>
2019-11-24 11:10:15 -0800

    adds solution to fib.js

---
+++ fib.js
@@ -0,0 +1,13 @@
+//Write a recursive function called fib which accepts a number
+//and returns the nth number in the fibonacci sequence
+
+
+function fib(n){
+    if (n<=2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
