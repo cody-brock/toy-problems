eade079831f33e7e3a4d013cc75f28497b39f65e
eternal golden bread <codylewisbrock@gmail.com>
2020-04-06 19:25:36 -0700

    adds work and solution to the react color-boxes activity

---
+++ react-toy-problems/color-boxes/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BoxesContainer from './BoxesContainer';
+import Box from './Box';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxesContainer />
+    </div>
+  );
+}
+
+export default App;
