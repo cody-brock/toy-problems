86d911c4181db266a03e983bc27ab8c00fe80cc9
eternal golden bread <codylewisbrock@gmail.com>
2020-04-06 18:48:25 -0700

    adds styling and improves logic behinnd coin-flipper activity

--- react-toy-problems/coin-flipper/src/Flipper.js
+++ react-toy-problems/coin-flipper/src/Flipper.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
+import Coin from './Coin';
+import { choice } from './helpers'
 
 class Flipper extends Component {
-  // static defaultProps = {
-    
-  // }
+
+  static defaultProps = {
+    coins: [
+      { side: 'heads', imgSrc: 'http://tinyurl.com/react-coin-heads-jpg' },
+      { side: 'tails', imgSrc: 'http://tinyurl.com/react-coin-tails-jpg' },
+    ]
+  }
+
   constructor(props) {
     super(props);
-    this.state = { side: 'heads', numClicks: 0, numHeads: 0, numTails: 0}
+    this.state = { 
+      currCoin: null, 
+      imageSource: props.coins[0].imgSrc,
+      numClicks: 0, 
+      numHeads: 0, 
+      numTails: 0
+    }
     this.handleClick = this.handleClick.bind(this);
     this.flipCoin = this.flipCoin.bind(this);
   }
 
   flipCoin() {
-    let randNum = Math.random()
-    if (randNum > .5) {
-      this.setState(st => {
-        return { side: 'heads', numHeads: st.numHeads + 1 };
-      });
-    } else {
-      this.setState(st => {
-        return { side: 'tails', numTails: st.numTails + 1 };
-      });
-    }
+    const newCoin = choice(this.props.coins);
+    this.setState(st => {
+      return {
+        currCoin: newCoin,
+        numClicks: st.numClicks + 1,
+        numHeads: st.numHeads + (newCoin.side === "heads" ? 1 : 0),
+        numTails: st.numTails + (newCoin.side === "tails" ? 1 : 0),
+      }
+    });
   }
 
   handleClick() {
-    this.setState(st => {
-      return { numClicks: st.numClicks + 1 };
-    });
     this.flipCoin();
   }
 
@@ -36,6 +45,7 @@ class Flipper extends Component {
       <div>
         <h1>Let's flip a coin...</h1>
         <p>{this.state.side}</p>
+        {this.state.currCoin && <Coin info={this.state.currCoin} />}
         <p>Out of {this.state.numClicks} flips, there have been {this.state.numHeads} heads and {this.state.numTails} tails</p>
         <button onClick={this.handleClick}>Flip Again!</button>
       </div>
