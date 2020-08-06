b3f33fd37ef2c17ff07eb9d2c09bb1be780de3c8
eternal golden bread <codylewisbrock@gmail.com>
2020-08-06 14:52:46 -0700

    adds solution to integer to roman leetcode problem

---
+++ integerToRoman.js
@@ -0,0 +1,40 @@
+// /**
+//  * @param {number} num
+//  * @return {string}
+//  */
+var intToRoman = function(num) {
+  var ans = [];
+  var obj = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+  }
+  
+  while (num > 0) {
+      for (let letter in obj) {
+          if (num >= obj[letter]) {
+              ans.push(letter);
+              num = num - obj[letter];
+              break;
+          }
+      }
+  }
+  
+  return ans.join('')
+  
+};
+
+console.log("Hello")
+console.log(intToRoman(55));
+console.log(intToRoman(1009));
+console.log(intToRoman(735));
