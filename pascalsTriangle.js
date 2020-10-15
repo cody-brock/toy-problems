ed807f2104ab7fb1a52b0f7ecf2ece41c1242cc8
eternal golden bread <codylewisbrock@gmail.com>
2020-10-15 10:53:44 -0700

    another solution to the Pascals Triangle generator problem

--- pascalsTriangle.js
+++ pascalsTriangle.js
@@ -30,3 +30,24 @@ var generate = function(numRows) {
   }
   return res;
 };
+
+var generate2 = function(numRows) {
+  if (numRows <= 0) return [];
+  
+  let pascal = [];
+  
+  for (let i = 0; i < numRows; i++) {
+      
+      pascal[i] = [];
+      pascal[i][0] = 1
+      
+      for (let j = 1; j < i; j++) {
+          pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+      }
+      pascal[i][i] = 1;
+      
+  }
+  
+  return pascal;
+  
+};
