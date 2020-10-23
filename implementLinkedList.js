3ae22a4f06012befe9e6f5ec32831676613538b4
eternal golden bread <codylewisbrock@gmail.com>
2020-10-22 20:46:12 -0700

    continues work on singly linked lists

--- implementLinkedList.js
+++ implementLinkedList.js
@@ -1,52 +1,83 @@
 class Node {
-  constructor(element) {
-    this.value = element
+  constructor(val) {
+    this.val = val;
     this.next = null;
   }
 }
 
-class LinkedList {
+class SinglyLinkedList {
   constructor() {
     this.head = null;
-    this.size = 0;
+    this.tail = null;
+    this.length = 0;
   }
 
-  appendToTail(element) {
-    let node = new Node(element);
-    let current;
-    if (this.head === null) {
-      this.head = node;
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
     } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
       current = this.head;
-
-      while (current.next) {
-        current = current.next;
-      }
-
-      current.next = node;
     }
-    this.size++;
-  }
-
-  deleteNode(element) {
-    let n = this.head;
-    console.log("N", n);
-
-    if (n.value === element) {
-      return this.next;
-    }
-
-    while (n.next !== null) {
-      if (n.next.value === element) {
-        n.next = n.next.next;
-        return this;
-      }
-      n = n.next;
-    }
+    this.length++;
     return this;
   }
 
-  // Other methods...
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    this.head = current.next;
+    this.size--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return current;
+  }
+
+  getIndex(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter < index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+
+  removeIndex(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length) return this.pop();
+
+    let previousNode = this.getIndex(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
 
 }
 
@@ -54,6 +85,8 @@ let list1 = new LinkedList();
 console.log(list1);
 list1.appendToTail(2);
 list1.appendToTail(8);
+list1.appendToTail(3);
+list1.appendToTail(7);
 console.log(list1);
 list1.deleteNode(2);
 console.log(list1);
