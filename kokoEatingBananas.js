0b7584d561fd14e3aa14e30cec9c8627fa7408e9
eternal golden bread <codylewisbrock@gmail.com>
2020-11-10 16:20:43 -0800

    reworks for a better solution using binary search.  Accepted.

--- kokoEatingBananas.js
+++ kokoEatingBananas.js
@@ -32,4 +32,40 @@ var minEatingSpeed = function(piles, H) {
 };
 
 
+
 // Better solution
+var minEatingSpeed = function(piles, H) {
+    
+  // Edge cases
+  if (piles.length < 1) return null;
+  if (H < 1) return null;
+  if (piles.length > H) return null;
+  if (piles.length === H) return Math.max(...piles);
+  
+  // Helper function - returns true if all the bananas in piles can be eaten in time using that speed
+  const canEat = function(speed) {
+      let time = 0;
+      for (pile of piles) {
+          time += Math.ceil(pile / speed);
+      }
+      return time <= H
+  }
+  
+  
+  // Two pointer approach
+  let l = 1;
+  let r = Math.max(...piles);
+  
+  // Uses binary search to find the min speed between 1 and piles' max val
+  while (l < r) {
+      let m = Math.floor((l + r) / 2);
+      if (canEat(m)) {
+          r = m;
+      } else {
+          l = m + 1;
+      }
+  }
+  
+  return l;
+  
+};
