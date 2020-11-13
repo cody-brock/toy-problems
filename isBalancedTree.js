d439ee6a2edb798abcc28058f5eae30526fe25d1
eternal golden bread <codylewisbrock@gmail.com>
2020-11-12 16:00:36 -0800

    returns to tree balancing problem, finding a better solution

--- isBalancedTree.js
+++ isBalancedTree.js
@@ -46,3 +46,18 @@ var isBalanced = function(root) {
   return flag;
   
 };
+
+// Faster solution
+var isBalanced = function(root) {
+    
+    let dfs = function(node) {
+        if (node === null) return 0;
+        let left = 1 + dfs(node.left);
+        let right = 1 + dfs(node.right);
+        if (Math.abs(left - right) > 1) return Infinity;
+        return Math.max(left, right);
+    }
+    
+    return dfs(root) === Infinity ? false : true;
+    
+};
