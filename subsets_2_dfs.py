70aac354ebc82b4c357f0ae1f9c5377a00318389
Cody Brock <codylewisbrock@gmail.com>
2021-07-22 09:58:45 -0700

    Adds an additional solution. Surprised that it isnt that much faster than the first one.

--- subsets_2_dfs.py
+++ subsets_2_dfs.py
@@ -14,3 +14,22 @@ class FirstSolution(object):
         res.append(sorted(path))
         for i in range(len(nums)):
             self.dfs(nums[i+1:], path+[nums[i]], res)
+
+class SecondSolution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(sorted(nums), [], res)
+        return res
+        
+        
+    def dfs(self, nums, path, res):
+        res.append(path)
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            self.dfs(nums[i+1:], path+[nums[i]], res)
