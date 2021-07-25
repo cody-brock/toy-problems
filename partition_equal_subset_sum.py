5c1488e9a4f2c55cffd38987d5da09a1e1363110
Cody Brock <codylewisbrock@gmail.com>
2021-07-25 10:14:51 -0700

    Adds improved versino of solution.  Caching was the key

--- partition_equal_subset_sum.py
+++ partition_equal_subset_sum.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution_First(object):
     def canPartition(self, nums):
         """
         :type nums: List[int]
@@ -23,3 +23,34 @@ class Solution(object):
             
         
         return dfs(target, 0)
+
+
+
+# Runtime: 92 ms, faster than 97.03% of Python online submissions for Partition Equal Subset Sum.
+# Memory Usage: 13.9 MB, less than 74.42% of Python online submissions for Partition Equal Subset Sum.
+class Solution_Improved(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        target, remainder = divmod(sum(nums), 2)
+        if remainder:
+            return False
+        # nums = sorted(nums)
+        
+        def dfs(nums, target, cache):
+            
+            if target < 0: return False
+            if target == 0: return True
+            if target in cache: return False
+            cache.add(target)
+            
+            for idx, val in enumerate(nums):
+                if dfs(nums[idx+1:], target-val, cache):
+                    return True
+            return False
+        
+
+        return dfs(nums, target, set())
