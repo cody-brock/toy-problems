82efa117492e7b8498f7d5d7d20f3592fe47e93c
Cody Brock <codylewisbrock@gmail.com>
2021-07-28 10:07:08 -0700

    Adds initial solution for binary tree inorder traversal problem, this time in Python.  Not very performant.

---
+++ binary_tree_inorder_traversal.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+        if not root or root == None:
+            return []
+
+        stack = []
+        res = []
+        curr = root
+        
+        while stack or curr:
+            
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+        
+        return res
