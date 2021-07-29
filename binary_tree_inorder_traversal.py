1c5ee10954ded5c13f566568d97e746c6cbed8db
Cody Brock <codylewisbrock@gmail.com>
2021-07-28 23:51:04 -0700

    Adds another recursive solution, without use of helper.  Strange, but works.

--- binary_tree_inorder_traversal.py
+++ binary_tree_inorder_traversal.py
@@ -55,3 +55,22 @@ class SolutionRecursive(object):
         helper(root)
         
         return res
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class SolutionRecursiveAgain(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+        if not root:
+            return []
+        else:
+            return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right)
