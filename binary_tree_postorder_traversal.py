f49611f20ae6e2991b4abc0e23011eacf6a52091
Cody Brock <codylewisbrock@gmail.com>
2021-07-29 00:22:34 -0700

    Adds a solution using regular recursion

--- binary_tree_postorder_traversal.py
+++ binary_tree_postorder_traversal.py
@@ -27,3 +27,14 @@ class RecursiveSolutionWithHelper(object):
         return res
 
 
+
+class RecursiveSolution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        else:
+            return self.postorderTraversal(root.left)+self.postorderTraversal(root.right)+[root.val]
